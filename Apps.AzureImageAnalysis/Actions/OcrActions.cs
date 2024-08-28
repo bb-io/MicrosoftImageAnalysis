7d4481c9f0a13497f5122dc3b360f1627130597f
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-08-28 16:54:06 +0300

    Added logging

--- Apps.AzureImageAnalysis/Actions/OcrActions.cs
+++ Apps.AzureImageAnalysis/Actions/OcrActions.cs
@@ -6,53 +6,69 @@ using Apps.AzureImageAnalysis.Models.Response;
 using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Actions;
 using Blackbird.Applications.Sdk.Common.Invocation;
-using Blackbird.Applications.SDK.Extensions.FileManagement.Interfaces;
 using Blackbird.Applications.Sdk.Utils.Extensions.Http;
 using RestSharp;
 
 namespace Apps.AzureImageAnalysis.Actions;
 
 [ActionList]
-public class OcrActions(InvocationContext invocationContext, IFileManagementClient fileManagementClient)
+public class OcrActions(InvocationContext invocationContext)
     : AzureImageAnalysisInvocable(invocationContext)
 {
     [Action("Recognize text", Description = "Recognize text in the specified image or document")]
     public async Task<RecognizeTextResponse> RecognizeText(
         [ActionParameter] RecognizeTextRequest input)
     {
-        var request = new AzureImageAnalysisRequest(ApiEndpoints.Ocr, Method.Post, Creds)
-            .WithJsonBody(new
+        try
+        {
+            var request = new AzureImageAnalysisRequest(ApiEndpoints.Ocr, Method.Post, Creds)
+                .WithJsonBody(new
+                {
+                    url = input.File.Url ?? throw new Exception("File URL is required"),
+                });
+
+            if (!string.IsNullOrEmpty(input.Language))
             {
-                url = input.File.Url
-            });    
-        
-        if (!string.IsNullOrEmpty(input.Language))
-        {
-            request = request.AddQueryParameter("language", input.Language);
+                request = request.AddQueryParameter("language", input.Language);
+            }
+
+            if (input.Pages != null)
+            {
+                var pages = string.Join(",", input.Pages);
+                request = request.AddQueryParameter("pages", pages);
+            }
+
+            var response = await Client.ExecuteWithErrorHandling(request);
+            var operationLocation = response.Headers!.FirstOrDefault(x => x.Name == ApiHeaders.OperationLocation)?.Value
+                                        ?.ToString()
+                                    ?? throw new Exception("Operation-Location header not found in response");
+            
+            await Logger.LogAsync(new
+            {
+                input,
+                operationLocation
+            });
+            
+            var latestGuid = operationLocation.Split('/').Last();
+
+            ReadTextEntity readTextEntity;
+            do
+            {
+                var resultRequest =
+                    new AzureImageAnalysisRequest($"{ApiEndpoints.OcrResult}/{latestGuid}", Method.Get, Creds);
+                readTextEntity = await Client.ExecuteWithErrorHandling<ReadTextEntity>(resultRequest);
+                await Task.Delay(3000);
+            } while (readTextEntity.Status == "running" || readTextEntity.Status == "notStarted");
+
+            if (readTextEntity.Status == "failed")
+                throw new Exception("The operation has failed.");
+
+            return new(readTextEntity);
         }
-        
-        if (input.Pages != null)
+        catch (Exception e)
         {
-            var pages = string.Join(",", input.Pages);
-            request = request.AddQueryParameter("pages", pages);
+            await Logger.LogErrorAsync(e);
+            throw;
         }
-        
-        var response = await Client.ExecuteWithErrorHandling(request);
-        var operationLocation = response.Headers!.FirstOrDefault(x => x.Name == ApiHeaders.OperationLocation)?.Value?.ToString()
-            ?? throw new Exception("Operation-Location header not found in response");
-        var latestGuid = operationLocation.Split('/').Last();
-        
-        ReadTextEntity readTextEntity;
-        do
-        {
-            var resultRequest = new AzureImageAnalysisRequest($"{ApiEndpoints.OcrResult}/{latestGuid}", Method.Get, Creds);
-            readTextEntity = await Client.ExecuteWithErrorHandling<ReadTextEntity>(resultRequest);
-            await Task.Delay(3000);
-        } while (readTextEntity.Status == "running" || readTextEntity.Status == "notStarted");
-        
-        if(readTextEntity.Status == "failed")
-            throw new Exception("The operation has failed.");
-        
-        return new(readTextEntity);
     }
 }
